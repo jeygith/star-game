f81e3c03f0e7dafb80823a4d3be4491ecf0005ad
Jeff Githiri <geoffrey.githiri@andela.com>
2019-12-05 14:42:52 +0300

    extract number and stars display components

--- src/App.js
+++ src/App.js
@@ -8,8 +8,25 @@ import './App.css';
 //STAR MATCH - Starting Template
 import ReactDOM from 'react-dom';
 
+const StarsDisplay = (props) => (
+
+    <>
+        {utils.range(1, props.count).map(starId =>
+            <div key={starId} className="star"/>
+        )}
+    </>
+);
+
+const PlayNumber = (props) => (
+    <button className="number" onClick={() => console.log('Num', props.number)}>
+        {props.number}
+    </button>
+
+);
+
+
 const StarMatch = () => {
-    const [stars, setState] = useState( utils.random(1, 9));
+    const [stars, setState] = useState(utils.random(1, 9));
     return (
         <div className="game">
             <div className="help">
@@ -17,13 +34,11 @@ const StarMatch = () => {
             </div>
             <div className="body">
                 <div className="left">
-                    {utils.range(1, stars).map(starId =>
-                        <div key={starId} className="star"/>
-                    )}
+                    <StarsDisplay count={stars}/>
                 </div>
                 <div className="right">
                     {utils.range(1, 9).map(number =>
-                        <button key={number} className="number">{number}</button>
+                        <PlayNumber key={number} number={number}/>
                     )}
                 </div>
             </div>
